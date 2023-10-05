8b8b7baa594057556dfef6977c601876b283709b
Dig092 <diggupta092@gmail.com>
2023-10-05 10:58:03 +0530

    added local storage function

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const miliSec = document.getElementsByClassName("mili-sec")[0];
 const lapList = document.getElementsByClassName("lapList")[0];
 const clearall = document.getElementsByClassName("clearAll")[0];
 
-
 let isPlay = false;
 let min = 0;
 let sec = 0;
@@ -14,52 +13,19 @@ let milisec = 0;
 let interval;
 let lapCount = 0;
 
-const toggle = () => {
-    if(!isPlay){
-        start.innerHTML = 'Stop';
-        interval = playmiliSec();
-        isPlay = true;
-    }else{
-        start.innerHTML = 'Start';  
-        clearInterval(interval);
-        isPlay = false  ;
+// Function to retrieve lap data from local storage and populate lap list
+const loadLapsFromLocalStorage = () => {
+    const storedLapData = localStorage.getItem('lapData');
+    if (storedLapData) {
+        const lapData = JSON.parse(storedLapData);
+        lapData.forEach((lap) => {
+            addLapToUI(lap);
+        });
     }
-}
+};
 
-const playmiliSec = () => {
-    return setInterval(() => {
-        if(milisec < 100){
-            miliSec.innerHTML = ++milisec;
-        }else{
-            milisec = 0;
-            if(sec < 60){
-                second.innerHTML = ++sec;
-            }else{
-                sec = 0;
-                if(min < 60){
-                    minutes.innerHTML = ++min;
-                }else{
-                    min = 0;
-                }
-            }    
-        } 
-    },10)
-}
-
-document.addEventListener('keydown', function(event){
-    if(event.key === 'R'){
-        minutes.innerHTML = 00;
-        second.innerHTML = 00;
-        miliSec.innerHTML = 00;
-    } 
-})
-document.addEventListener('keydown', function(event){
-    if(event.key === 'c'){
-        lapList.innerHTML = '';
-    } 
-})
-
-const lap = () => {
+// Function to add a lap to the UI
+const addLapToUI = (lap) => {
     const li = document.createElement("li");
     const number = document.createElement("span");
     const timeStamp = document.createElement("span");
@@ -68,12 +34,87 @@ const lap = () => {
     number.setAttribute("class", "lapCount");
     timeStamp.setAttribute("class", "lapTime");
 
-    number.innerHTML =  `#${++lapCount}`;
-    timeStamp.innerHTML = `${min} : ${sec} : ${milisec}`;
+    number.innerHTML = lap.number;
+    timeStamp.innerHTML = lap.timeStamp;
 
     li.append(number, timeStamp);
     lapList.append(li);
-}
+};
+
+// Function to store lap data in local storage
+const storeLapInLocalStorage = (lap) => {
+    const storedLapData = localStorage.getItem('lapData');
+    let lapData = [];
+    if (storedLapData) {
+        lapData = JSON.parse(storedLapData);
+    }
+    lapData.push(lap);
+    localStorage.setItem('lapData', JSON.stringify(lapData));
+};
+
+const toggle = () => {
+    if (!isPlay) {
+        start.innerHTML = 'Stop';
+        interval = playmiliSec();
+        isPlay = true;
+    } else {
+        start.innerHTML = 'Start';
+        clearInterval(interval);
+        isPlay = false;
+    }
+};
+
+const playmiliSec = () => {
+    return setInterval(() => {
+        if (milisec < 100) {
+            miliSec.innerHTML = ++milisec;
+        } else {
+            milisec = 0;
+            if (sec < 60) {
+                second.innerHTML = ++sec;
+            } else {
+                sec = 0;
+                if (min < 60) {
+                    minutes.innerHTML = ++min;
+                } else {
+                    min = 0;
+                }
+            }
+        }
+    }, 10);
+};
+
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'R') {
+        minutes.innerHTML = '00';
+        second.innerHTML = '00';
+        miliSec.innerHTML = '00';
+    }
+});
+
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'c') {
+        lapList.innerHTML = '';
+        // Clear lap data from local storage when clearing the list
+        localStorage.removeItem('lapData');
+    }
+});
+
+const lap = () => {
+    const lapData = {
+        number: `#${++lapCount}`,
+        timeStamp: `${min}:${sec}:${milisec}`,
+    };
+    
+    // Add lap data to UI
+    addLapToUI(lapData);
+    
+    // Store lap data in local storage
+    storeLapInLocalStorage(lapData);
+};
+
+// Load lap data from local storage when the page loads
+loadLapsFromLocalStorage();
 
 start.addEventListener("click", toggle);
 lapbtn.addEventListener("click", lap);
